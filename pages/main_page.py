c26a70e25f23a7e7e291adf19919a591bcd83db7
polinach <chukhninap@gmail.com>
2022-10-28 16:19:53 +0300

    add test go_to_login_page

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,5 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
-from selenium.webdriver.common.by import By
 
 
 class MainPage(BasePage):
@@ -10,4 +9,3 @@ class MainPage(BasePage):
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
-
