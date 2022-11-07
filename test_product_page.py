d07949eb63aa3cea8a0d6d42e37ebf2208953ac7
polinach <chukhninap@gmail.com>
2022-11-07 15:26:43 +0400

    prepare for review

--- test_product_page.py
+++ test_product_page.py
@@ -51,13 +51,14 @@ class TestUserAddToBasketFromProductPage:
         browser.implicitly_wait(15)
         login_page.should_be_authorized_user()
 
-    def test_guest_can_add_product_to_basket(self, browser):
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_basket(self, browser):
         link = Links.PRODUCT_PAGE_LINK
         product_page = ProductPage(browser, link)
         product_page.open()
         product_page.add_product_to_basket()
 
-    def test_guest_cant_see_success_message(self, browser):
+    def test_user_cant_see_success_message(self, browser):
         link = Links.PRODUCT_PAGE_LINK
         product_page = ProductPage(browser, link)
         product_page.open()
@@ -65,7 +66,15 @@ class TestUserAddToBasketFromProductPage:
 
 
 @pytest.mark.parametrize('link', urls)
-def test_guest_can_add_product_to_basket(browser, link):
+def test_guest_can_add_product_to_basket_parametrize(browser, link):
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_product_to_basket()
+
+
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket_parametrize(browser):
+    link = Links.PRODUCT_PAGE_LINK
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_product_to_basket()
@@ -94,6 +103,7 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     product_page.should_disappear_success_message()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = Links.PRODUCT_PAGE_LINK
     product_page = ProductPage(browser, link)
@@ -101,3 +111,20 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     product_page.go_to_basket()
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_empty_basket()
+
+
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = Links.PRODUCT_PAGE_LINK
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
+
+def test_guest_should_see_login_link(browser):
+    link = Links.PRODUCT_PAGE_LINK
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_be_login_link()
