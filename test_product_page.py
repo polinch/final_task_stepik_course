d672812e57625d5a76ffd6a3a2292c7ee9863d6e
polinach <chukhninap@gmail.com>
2022-11-07 12:34:37 +0400

    add class TestLoginFromProductPage

--- test_product_page.py
+++ test_product_page.py
@@ -13,7 +13,19 @@ urls = [f"{product_base_link}?promo=offer{no}" if no != 7
 
 @pytest.mark.login
 class TestLoginFromProductPage:
-    pass
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.should_be_login_link()
 
 
 @pytest.mark.parametrize('link', urls)
@@ -45,22 +57,6 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     product_page.open()
     product_page.add_product_to_basket()
     product_page.should_disappear_success_message()
-
-
-def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.should_be_login_link()
-
-
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
