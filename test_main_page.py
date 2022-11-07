8daac5f65ab0fb295ba967ecfeaaf6ba7773a54d
polinach <chukhninap@gmail.com>
2022-11-07 12:21:14 +0400

    add class TestMainPage for login page's cases

--- test_main_page.py
+++ test_main_page.py
@@ -8,26 +8,18 @@ from .pages.basket_page import BasketPage
 @pytest.mark.login_guest
 class TestMainPage:
     def test_guest_can_go_to_login_page(self, browser):
-        pass
+        link = "http://selenium1py.pythonanywhere.com/"
+        main_page = MainPage(browser, link)
+        main_page.open()
+        main_page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
     def test_guest_should_see_login_link(self, browser):
-        pass
-
-
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    main_page = MainPage(browser, link)
-    main_page.open()
-    main_page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
-
-
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    main_page = MainPage(browser, link)
-    main_page.open()
-    main_page.should_be_login_link()
+        link = "http://selenium1py.pythonanywhere.com/"
+        main_page = MainPage(browser, link)
+        main_page.open()
+        main_page.should_be_login_link()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
