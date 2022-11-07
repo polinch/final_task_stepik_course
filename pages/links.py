1cb30c11e9c20813f49a832de2434da50c7a3bb4
polinach <chukhninap@gmail.com>
2022-11-07 14:10:25 +0400

    add links' file

---
+++ pages/links.py
@@ -0,0 +1,4 @@
+class Links:
+    MAIN_PAGE_LINK = "http://selenium1py.pythonanywhere.com/"
+    PRODUCT_PAGE_LINK = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    PRODUCT_PAGE_PARAMETER_LINK = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
--- test_main_page.py
+++ test_main_page.py
@@ -3,12 +3,13 @@ import pytest
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
+from .pages.links import Links
 
 
 @pytest.mark.login_guest
 class TestMainPage:
     def test_guest_can_go_to_login_page(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/"
+        link = Links.MAIN_PAGE_LINK
         main_page = MainPage(browser, link)
         main_page.open()
         main_page.go_to_login_page()
@@ -16,14 +17,14 @@ class TestMainPage:
         login_page.should_be_login_page()
 
     def test_guest_should_see_login_link(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/"
+        link = Links.MAIN_PAGE_LINK
         main_page = MainPage(browser, link)
         main_page.open()
         main_page.should_be_login_link()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = Links.MAIN_PAGE_LINK
     main_page = MainPage(browser, link)
     main_page.open()
     main_page.go_to_basket()
